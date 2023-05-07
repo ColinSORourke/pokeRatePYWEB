90525244ea07d99cbc6ea3a953528af2d59085fc
Colin O'Rourke <csorourk@ucsc.edu>
2023-05-07 00:19:06 -0700

    Added rudimentary guessing

--- static/js/puzzle.js
+++ static/js/puzzle.js
@@ -9,6 +9,15 @@ let init = (app) => {
     // This is the Vue data.
     app.data = {
         // Complete as you see fit.
+        myDexJSON: dexJSON,
+        myPokemon: dexJSON["Pokemon"].filter((pokemon) => 
+            pokemon.form == "Basic"
+        ),
+        query: "",
+        acSuggestions: [],
+        myGuesses: [],
+        mostRecentGuess: {name: "None"},
+        noGuess: true,
     };
 
     app.enumerate = (a) => {
@@ -21,13 +30,47 @@ let init = (app) => {
 
     // This contains all the methods.
     app.methods = {
-
+        onchange() {
+            if (app.vue.query != ""){
+                app.vue.acSuggestions = app.vue.myPokemon.filter((pokemon) => pokemon.name.toLowerCase().indexOf(app.vue.query.toLowerCase()) == 0).slice(0,5) 
+            } else {
+                app.vue.acSuggestions = [];
+            }
+            
+        },
+        acFill(p) {
+            app.vue.query = p.name;
+        },
+        submitGuess(){
+            let guess = app.vue.myPokemon.find(p => p.name == app.vue.query);
+            if (guess != undefined){
+                app.vue.myGuesses.push(guess);
+                app.vue.mostRecentGuess = guess;
+                app.vue.noGuess = false;
+                app.vue.query = '';
+            }
+        },
+        pokemonImagePath(p) {
+            return "images/PokemonArt/" + p['generation'] + "/" + p["fullname"].replace("\u2640", "Female").replace("\u2642", "Male").replaceAll("\u00e9", "e")  + ".png";
+        },
+        pokemonNumber(p) {
+            return "#" + p['number'];
+        },
+        typeImagePath(p, i) {
+            return "images/Types/" + p.types[i] + "_en.png";
+        },
+        widthPerc(p){
+            if (p.globalAverage == -1){
+                return "width: 50%;";
+            }
+            return "width: " + p.globalAverage*20 + "%;"
+        },
     }
     // This creates the Vue instance.
     app.vue = new Vue({
         el: "#vue-target",
         data: app.data,
-        methods: app.methods
+        methods: app.methods,
     });
 
     // And this initializes it.
